32348d736734b28f725a898fb672abf546cb0243
osakana0630 <osakana0630@gmail.com>
2021-01-03 02:15:51 +0900

    修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # 実装は終わっていないことに注意!
+    @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
       # 以下の処理はredirect_to user_url(@user)と等価
